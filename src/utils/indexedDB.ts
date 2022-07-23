ff07841e4690cc6e0576b2cbc55fbdb7343bfe58
sunpm <15022352@qq.com>
2022-07-23 15:32:44 +0800

    Typescript封装工具类：增、删、改、查

--- src/utils/indexedDB.ts
+++ src/utils/indexedDB.ts
@@ -1,5 +1,6 @@
 export default class DB {
   private readonly dbName: string // 数据库名称
+  private db: any // 数据库对象
 
   constructor(dbName: string) {
     this.dbName = dbName
@@ -8,7 +9,8 @@ export default class DB {
   // 打开数据库
   openStore(storeName: string, keyPath: string, indexs: string[] = []) {
     const request = window.indexedDB.open(this.dbName, 1)
-    request.onsuccess = event => {
+    request.onsuccess = (event: any) => {
+      this.db = event.target.result
       console.log('数据库打开成功')
       console.log(event)
     }
@@ -26,7 +28,7 @@ export default class DB {
       })
 
       indexs.length && indexs.forEach((index: string) => {
-        store.createIndex(index, index, { unique: true })
+        store.createIndex(index, index, { unique: false })
       })
 
       store.transaction.oncomplete = (event: any) => {
@@ -36,5 +38,65 @@ export default class DB {
       console.log('数据库升级成功')
       console.log(event)
     }
+  }
+
+  //  新增/修改数据库数据
+  updateItem(storeName: string, data: any) {
+    const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+    const request = store.put({ ...data, updateTime: new Date().getTime() })
+    request.onsuccess = (event: any) => {
+      console.log('数据写入成功')
+      console.log(event)
+    }
+    request.onerror = (event: any) => {
+      console.log('数据写入失败')
+      console.log(event)
+    }
+  }
+
+  //  删除数据库数据
+  deleteItem(storeName: string, key: number | string) {
+    const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+    const request = store.delete(key)
+    request.onsuccess = (event: any) => {
+      console.log('数据删除成功')
+      console.log(event)
+    }
+    request.onerror = (event: any) => {
+      console.log('数据删除失败')
+      console.log(event)
+    }
+  }
+
+  // 查询所有数据库数据
+  getList(storeName: string) {
+    const store = this.db.transaction(storeName).objectStore(storeName)
+    const request = store.getAll()
+    request.onsuccess = (event: any) => {
+      console.log('查询所有数据成功')
+      console.log(event.target.result)
+    }
+    request.onerror = (event: any) => {
+      console.log('查询所有数据失败')
+      console.log(event)
+    }
+  }
+
+  // 查询某条数据
+  getItem(storeName: string, key: number | string) {
+    const store = this.db.transaction(storeName).objectStore(storeName)
+    const request = store.get(key)
+    return new Promise((resolve, reject) => {
+      request.onsuccess = (event: any) => {
+        console.log('查询某一条数据成功')
+        console.log(event.target.result)
+        resolve(event.target.result)
+      }
+      request.onerror = (event: any) => {
+        console.log('查询某一条数据失败')
+        console.log(event)
+        reject(event)
+      }
+    })
   }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -25,17 +25,45 @@ const getRoomList = () => {
 
 getRoomList()
 
+// 数据库相关操作
 const airbnbDB = new IndexedDB('airbnb')
 
 airbnbDB.openStore('elephant', 'id', ['nose', 'ear'])
 
 const value1 = ''
+
+// 增 / 改
+function addDB(storeName: string) {
+  airbnbDB.updateItem(storeName, {
+    nose: '33m',
+    ear: '比较大'
+  })
+}
+
+// 删除
+function deleteDB(storeName: string, key: number | string) {
+  airbnbDB.deleteItem(storeName, key)
+}
+
+// 查询所有数据
+function getAll(storeName: string) {
+  airbnbDB.getList(storeName)
+}
+
+// 查询某一条数据
+function getItem(storeName: string, key: number | string) {
+  airbnbDB.getItem(storeName, key)
+}
 </script>
 
 <template>
   <div>
     国际化：{{ t('home') }}
   </div>
+  <button @click="addDB('elephant')">增</button>
+  <button @click="deleteDB('elephant', 1)">删除</button>
+  <button @click="getAll('elephant')">查询所有数据</button>
+  <button @click="getItem('elephant', 2)">查询id为2的数据</button>
   <button @click="() => router.push({path: '/mine', query: {id: 1}})">跳转到个人中心</button>
 
   <el-date-picker
